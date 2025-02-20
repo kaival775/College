dc34c0299cc12ab25f462147a69861e7f4f5301c
Vaibbs Parrotz <you@example.com>
2025-02-21 01:33:46 +0530

    Dice rolling by vaibbs (idea by aditya)

--- app/src/main/java/com/co/example/MainActivity.java
+++ app/src/main/java/com/co/example/MainActivity.java
@@ -1,6 +1,9 @@
 package com.co.example;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        final int[] dieStates = new int[]{
+                R.drawable.die_1,
+                R.drawable.die_2,
+                R.drawable.die_3,
+                R.drawable.die_4,
+                R.drawable.die_5,
+                R.drawable.die_6,
+        };
+
+        final Random rand = new Random();
+        final Animation anim = AnimationUtils.loadAnimation(this, R.anim.press);
+
+        ImageButton ib = findViewById(R.id.dice);
+        ib.setOnClickListener(view -> {
+            ib.setBackgroundResource(dieStates[rand.nextInt(6)]);
+            ib.startAnimation(anim);
         });
     }
 }
